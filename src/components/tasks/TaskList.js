c69f802b6b9430c1d5ce8c1879503d2812308510
tonykevin <hi@tonykevin.com>
2020-03-09 12:37:05 -0500

    initialize tasks state

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -7,12 +7,7 @@ const TaskList = () => {
   const { project, deleteProject } = useContext(projectContext)
   if (!project) { return <h2>Selecciona un proyecto</h2> }
 
-  const tasks = [
-    { id: 1, name: 'Elegir plataforma', state: true },
-    { id: 2, name: 'Elegir colores', state: false },
-    { id: 3, name: 'Elegir plataforma de pago', state: false },
-    { id: 4, name: 'Elegir Hosting', state: true }
-  ]
+  const tasks = []
 
   // Delete a project
   const onClickDelete = () => {
--- src/context/tasks/TaskState.js
+++ src/context/tasks/TaskState.js
@@ -5,13 +5,32 @@ import TaskReducer from './TaskReducer'
 
 const TaskState = props => {
   const initialState = {
-    tasks: []
+    tasks: [
+      { name: 'Elegir plataforma', state: true, projectId: 1 },
+      { name: 'Elegir colores', state: false, projectId: 2 },
+      { name: 'Elegir plataforma de pago', state: false, projectId: 3 },
+      { name: 'Elegir Hosting', state: true, projectId: 2 },
+      { name: 'Elegir plataforma de pago', state: true, projectId: 2 },
+      { name: 'Elegir colores', state: false, projectId: 3 },
+      { name: 'Elegir Hosting', state: true, projectId: 1 },
+      { name: 'Comprar dominio', state: false, projectId: 3 },
+      { name: 'Comprar dominio', state: true, projectId: 1 },
+      { name: 'Diseñar logo', state: false, projectId: 1 },
+      { name: 'Diseñar logo', state: true, projectId: 2 },
+      { name: 'Diseñar logo', state: false, projectId: 3 },
+      { name: 'Eligir gestor de DB', state: false, projectId: 3 }
+    ]
   }
 
   const [state, dispatch] = useReducer(TaskReducer, initialState)
+  const { tasks } = state
 
   return (
-    <TaskContext.Provider>
+    <TaskContext.Provider
+      value={{
+        tasks
+      }}
+    >
       {props.children}
     </TaskContext.Provider>
   )
